df55b18ce90be01facac9a62be0433fa9647eaa2
Aleksandr62 <74791135+Aleksandr62@users.noreply.github.com>
2021-05-23 23:35:04 +0300

    update breadcrumbs (use event-bus)

--- src/public/js/comBreadcrumbs.js
+++ src/public/js/comBreadcrumbs.js
@@ -1,13 +1,14 @@
+import eventBus from './eventBus' // шина событий
+
 const comPath = {
     props: ['link', 'isLast'],
     data() {
         return {
-            delimiter: ' / ',
         }
     },
     template: `
     <div class="catalog-top__breadcrumbs">
-        <div class="catalog-top__link" :class="{select: isLast}" @click="$root.change(link.path)">{{link.name}}</div><span > {{ delimiter }} </span>
+        <div class="catalog-top__link" :class="{select: isLast}" @click="$root.change(link.path)">{{link.name}}</div><span>&nbsp;&nbsp;/&nbsp;&nbsp;</span>
     </div>
     `,
 };
@@ -15,15 +16,12 @@ const comPath = {
 const comBreadcrumbs = {
     data() {
         return {
+            path: null,
         }
     },
     components: { comPath },
     computed: {
-        path: function () {
-            return [{name: 'HOME', path: 'main',},
-            {name: this.$root.currentPage.group, path: this.$root.currentPage.group,},
-            {name: this.$root.currentPage.name, path: this.$root.currentPage.name,}]
-        },
+
         headerPage: function () {
             if (this.$root.currentComp === 'registration') return 'REGISTRATION';
             if (this.$root.currentComp === 'product') return 'NEW ARRIVALS';
@@ -31,8 +29,13 @@ const comBreadcrumbs = {
             if (this.$root.currentComp === 'cart-order') return 'SHOPPING CART';
         },
     },
-    mounted() {
-    },
+    created() {
+          eventBus.$on('breadcrumbs-item', (group, el) => {
+            this.path = [{name: 'HOME', path: 'main',},
+                {name: group, path: group,},
+                {name: el, path: el,}]
+          });      
+      },
     template: `
 <nav class="catalog-top center">
     <div class="catalog-top__header"><h3 class="catalog-top__article select">{{headerPage}}</h3></div>
